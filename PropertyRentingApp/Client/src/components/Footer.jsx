bb5e62599402b62de77fb940de3226afcea5289f
Sarvesh Kumar Mishra <sarveshkmishra119@gmail.com>
2025-12-23 08:38:50 +0530

    Delete PropertyRentingApp directory

--- PropertyRentingApp/Client/src/components/Footer.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Footer = () => (
-  <footer className="footer">
-    <p>© 2025 Property Rental Platform | Made with ❤️</p>
-  </footer>
-);
-
-export default Footer;
--- PropertyRentingApp/Server/routes/review.js
+++
@@ -1,7 +0,0 @@
-const router = require('express').Router();
-const { createReviews } = require('../controllers/reviewController');
-const { verifyToken } = require('../middleware/auth');    
-
-router.post('/', verifyToken, createReviews);
-
-module.exports = router;
